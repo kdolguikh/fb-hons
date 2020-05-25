33268b17ee4217d9d055f120133b04c45bb639c5
Kat <kdolguik@sfu.ca>
2020-05-24 20:44:22 -0700

    improved comments, listed relevant Gnarly scripts

--- subjTableHack.m
+++ subjTableHack.m
@@ -1,15 +1,97 @@
-%Cognitive Science Lab, Simon Fraser University
-%Author: Kat Dolguikh
-%Date: Jan 24 2019
-%Last Edit: april 6 I THINK WORKS
+    %  Author: Kat 
+    %  Date Created: Jan 24 /20
+    %  Last Edit: Apr 6 
+     
+    %  Cognitive Science Lab, Simon Fraser University 
+    %  Originally Created For: feedback
+      
+    %  Reviewed: 
+    %  Verified: 
+
+    
+    %  PURPOSE: 
+        % Gnarly-style data extraction from saved .mat TrialLvl and FixLvl
+        % tables. Used in 2020 COVID-19 quarantine to complete work from
+        % home (aka without SQL connectors)
+ 
+    
+    %  INPUT: 
+    
+%         experiment: experiment name
+
+%         measure: what data are we looking for. the current options are
+%           only measures I needed for my honours project that were not
+%           attainable in a more direct way. In the future, if we hope to
+%           use something like this, it should probably be expanded.
+%           current measure options:
+%                     'cond': condition, only relevant for feedback2 and
+%                     feedback3 (9 sec or 1 sec feedback condition). one
+%                     row per participant
+%                     'cp': subject's CP (first of 24 in a row correct
+%                     trials) aka point at which we can say they learned.
+%                     one row per participant.
+%                     'rt2': phase 2 duration aka how long stimulus was
+%                     onscreen before participant made a category
+%                     selection. one row per trial per participant.
+%                     'rt4': phase 4 duration aka how long feedback was
+%                     onscreen before participant moved on to next trial.
+%                     one row per trial per participant. 
+%                     'accuracy': 1 if participant answered correctly, 0 if
+%                     they answered incorrectly. one row per trial per
+%                     participant. 
+%                     'durp2': mean duration of fixations to features
+%                     (funcRelevance = 1, 2, or 3) during phase 2. one row
+%                     per trial per participant
+%                     'durp4': mean duration of fixations to features
+%                     (funcRelevance = 1, 2, or 3) during phase 4. one row
+%                     per trial per participant
+%                     'fc2': number of fixations to features (funcRelevance
+%                     = 1, 2, or 3) during phase 2. one row per trial per
+%                     participant
+%                     'fc4': number of fixations to features (funcRelevance
+%                     = 1, 2, or 3) during phase 4. one row per trial per
+%                     participant
+%                     'irrel2': number of fixations to irrrelevant feature 
+%                     (funcRelevance = 3) during phase 2. one row per 
+%                     trial per participant
+%                     'irrel4': number of fixations to irrrelevant feature 
+%                     (funcRelevance = 3) during phase 4. one row per 
+%                     trial per participant
+%                     'opt': gaze optimization during phase 2. one row per
+%                     trial per participant.
+%                     'opt4': gaze optimization during phase 4. one row per
+%                     trial per participant.
+%                     'p4button': time fixating  feedback buttons
+%                     (funcRelevance = 4, 5, 6, or 7) during phase 4. one
+%                     row per trial per participant.
+%                     'p4feature': time fixating stimulus features
+%                     (funcRelevance = 1, 2, or 3) during phase 4. one row
+%                     per trial per participant. this may be exactly the
+%                     same as fc4 (oops LOL)
+
+
+    
+    %  OUTPUT: depending on measure, either 2xn table where n = number of
+    %  subjects in experiment or 2xtn table where t = number of trials, n =
+    %  number of subjects. first column is subject number, second column is
+    %  measure value.
+    
+    %  Additional Scripts Used: 
+    
+    
+    %  Additional Comments: for each measure here, I list the Gnarly script (with args) that I
+        % was intending to replicate. all of Gnarly can be found at:
+        % Analyses/General/Gnarly. Then most of the scripts you will need to look
+        % at will be in the DataExtractor folder. 
+
 
 % originally created for: FEEDBACK
 
-%% my gnarly hack since i don't have SQL right now
 
 
-function data = subjTableHack(experiment, measure, varargin)
+function data = subjTableHack(experiment, measure)
 
+    % this is where the data is stored on kat's computer
     direc = strcat('Users/16132/Documents/lab/KAT/', experiment);
 
     load(strcat(direc, '/explvl.mat'));
@@ -23,20 +105,26 @@ function data = subjTableHack(experiment, measure, varargin)
 
     subjects = explvl.Subject;
 
-%% THIS IS MY  GNARLY SUBSTITUTE
 switch measure
 
     case 'cond'   
-        data = [subjects, explvl.FeedbackDuration];     % note format
+        % don't worry about gnarly for this one, we literally just want
+        % each subject and their condition
+        data = [subjects, explvl.FeedbackDuration];   
         
     case 'cp' 
-        data = [subjects, explvl.CP];   % note format
+        % don't worry about gnarly for this one, we literally just want
+        % each subject and their CP
+        data = [subjects, explvl.CP];   
         
     case 'rt2'      % pull straight from trial lvl
+        % Gnarly: RTExtractor (phase 2)
         response = [];
         sub = [];
         for i = 1:length(subjects)
            current = subjects(i); 
+           % luckily, Phase2RT is already a variable in trial level tables,
+           % making this one easy
            filtered = triallvl.Phase2RT(triallvl.Subject == current);
            this = triallvl.Subject(triallvl.Subject == current);
            sub = [sub; this];
@@ -49,6 +137,7 @@ switch measure
         sub = [];
         for i = 1:length(subjects)
            current = subjects(i); 
+           % same as for p2, Phase4RT is a variable in the tables
            filtered = triallvl.Phase4RT(triallvl.Subject == current);
            this = triallvl.Subject(triallvl.Subject == current);
            sub = [sub; this];
@@ -61,6 +150,8 @@ switch measure
         sub = [];
         for i = 1:length(subjects)
            current = subjects(i); 
+           % similar to p2, p4 rt, accuracy is just a column in trial level
+           % tables
            filtered = triallvl.TrialAccuracy(triallvl.Subject == current);
            this = triallvl.Subject(triallvl.Subject == current);
            sub = [sub; this];
@@ -69,7 +160,10 @@ switch measure
         data = [sub, acc];
         
     case 'durp2'   % fixation time ON FEATURES (average)
+        % for this measure, we want the mean duration of all fixations to
+        % stimulus features for each trial
         
+        % Gnarly: MeanTimeOnFeatures, phase 2, 'all3' (so the means are not separated by feature)
         sub = [];
         dur = [];
         
@@ -78,11 +172,14 @@ switch measure
            thisSub = fixlvl(fixlvl.Subject == current, :);
            maxTrial = max(triallvl.TrialID(triallvl.Subject == current));
            
-           for j = 1:maxTrial% for every trial for the current subject, find all fixations with funcRelevance = 1, 2, or 3
+           for j = 1:maxTrial
+               % for every trial for the current subject, find all fixations with funcRelevance = 1, 2, or 3
+               
                thisTrial = thisSub(thisSub.TrialID == j, :);
                p2 = thisTrial(thisTrial.TrialPhase == 2, :);
                p2 = p2.Duration(p2.funcRelevance > 0 & p2.funcRelevance < 4);
-               if isempty(p2)   % if there are none, pad with a NaN
+               if isempty(p2)  
+                   % if there are none, pad with a NaN
                    meandur = NaN;
                else
                    meandur = nanmean(p2);
@@ -95,7 +192,7 @@ switch measure
         data = [sub, dur];
         
     case 'durp4'        % same as above but p4 instead of p2
-        
+        % Gnarly: MeanTimeOnFeatures, phase 4, 'all3'
         sub = [];
         dur = [];
         
@@ -121,7 +218,11 @@ switch measure
         end
         data = [sub, dur];
         
-    case 'fc2'      % same as above, but counnt using length() instead of taking a mean
+    case 'fc2'      % same as above, but count using length() instead of taking a mean
+        % here we are counting the number of fixations to features during
+        % p2 of each trial
+        
+        % Gnarly: FixationCountExtractor, phase 2, 'allinone'
         
         sub = [];
         fc = [];
@@ -134,8 +235,14 @@ switch measure
            for j = 1:maxTrial
                thisTrial = thisSub(thisSub.TrialID == j, :);
                p2 = thisTrial(thisTrial.TrialPhase == 2, :);
+               
+                % p2 will be a vector of all the subject's fixations (that
+                % we care about, aka to features) during p2 in the current
+                % trial. the length of this vector is the number of
+                % fixations.
                p2 = p2.Duration(p2.funcRelevance > 0 & p2.funcRelevance < 4);
                if isempty(p2)
+                   % empty = length 0 = no fixations
                    fc2 = 0;
                else
                    fc2 = length(p2);
@@ -150,6 +257,7 @@ switch measure
         
     case 'fc4'      % same as above, but p4 instead of p2
         
+        % Gnarly: FixationCountExtractor, phase 4, 'allinone'
         sub = [];
         fc = [];
         
@@ -176,34 +284,47 @@ switch measure
         data = [sub, fc];
         
 
-    case 'irrel2'   % time on irrelevant features in p2
+
+    case 'irrel2'   
+        % time spent on irrelevant features in p2
         
+        % Gnarly: DurationExtractor, phase 2, 'relevance' column 3 (the
+        % relevance argument separates data by funcrelevance and outputs
+        % values for all 3 features...this measure here only cares about
+        % the last one aka irrelevan)
         sub = [];
         irrel = [];
         
-        for i = 1:length(subjects)  % same as dur2, except only for funcRelevance = 3
+        for i = 1:length(subjects)  
            current = subjects(i);
            thisSub = fixlvl(fixlvl.Subject == current, :);
            maxTrial = max(triallvl.TrialID(triallvl.Subject == current));
            
            for j = 1:maxTrial
+               % for each trial, we find all fixations to irrelevant
+               % features (funcRelevance = 3) and take their sum to get the
+               % total time spent looking at irrelevant features on that
+               % trial
                thisTrial = thisSub(thisSub.TrialID == j, :);
                p2 = thisTrial(thisTrial.TrialPhase == 2, :);
                p2 = p2.Duration(p2.funcRelevance == 3);
                if isempty(p2)
-                   meandur = NaN;
+                   currentIrrel = NaN;
                else
-                   meandur = nanmean(p2);
+                   currentIrrel = nansum(p2);
                end
                sub = [sub; current];
-               irrel = [irrel; meandur];
+               irrel = [irrel; currentIrrel];
            end
           
             
         end
         data = [sub, irrel];
         
-    case 'irrel4'   % same as above, just p4 instead of p2
+    case 'irrel4'   
+        % same as above, just p4 instead of p2
+        
+        % Gnarly: DurationExtractor, phase 4, 'relevance'
         
         sub = [];
         irrel = [];
@@ -218,20 +339,22 @@ switch measure
                p4 = thisTrial(thisTrial.TrialPhase == 4, :);
                p4 = p4.Duration(p4.funcRelevance == 3);
                if isempty(p4)
-                   meandur = NaN;
+                   currentIrrel = NaN;
                else
-                   meandur = nanmean(p4);
+                   currentIrrel = nansum(p4);
                end
                sub = [sub; current];
-               irrel = [irrel; meandur];
+               irrel = [irrel; currentIrrel];
            end
           
             
         end
         data = [sub, irrel];
         
+
     case 'opt'        % pulled the calculation straight from gnarly. 
-                        % this is optimization during p2
+                        
+        % Gnarly: OptimizationExtractor, phase 2, 
         sub = [];
         opt = [];
         
@@ -248,10 +371,21 @@ switch measure
                else
                       relevant = p2.Duration(p2.funcRelevance == 1 | p2.funcRelevance == 2);
                       irrelevant = p2.Duration(p2.funcRelevance == 3);
-
+    
+                      % we divide relevant by 2 here because are two
+                      % relevant features:1 irrelevant feature on every
+                      % trial. in optimization, we are looking for a
+                      % measure that gives us: -1 if fixations are entirely
+                      % to irrelevant features, 1 if fixations are entirely
+                      % to relevant features, 0 if they occur at chance
+                      % levels (i.e. 1/3 of the fixations to each of the
+                      % features). the way to make this work is to divide
+                      % relevant by two (write it out and check for
+                      % yourself if you want)
                       relevant = nansum(relevant)/2;    
                       irrelevant = nansum(irrelevant);
 
+                      % so opt is rel-irrel/modified total
                       optimization = (relevant - irrelevant)/(relevant + irrelevant);
                end
                
@@ -266,6 +400,7 @@ switch measure
         
     case 'opt4'             % same as above, just p4 instead of p2
         
+        %Gnarly: OptimizationExtractor, phase 4
         sub = [];
         opt = [];
         
@@ -275,6 +410,8 @@ switch measure
            maxTrial = max(triallvl.TrialID(triallvl.Subject == current));
            
            for j = 1:maxTrial
+               % this should be exactly the same as in 'opt', except using
+               % TrialPhase == 4 instead of ==2
                thisTrial = thisSub(thisSub.TrialID == j, :);
                p4 = thisTrial(thisTrial.TrialPhase == 4, :);
                if isempty(p4)
@@ -298,7 +435,10 @@ switch measure
         
         data = [sub, opt];
         
-    case 'p4button'     % similar to time on irrel
+    case 'p4button'     
+        % total time fixating on feedback buttons during phase 4 on each
+        % trial
+        % Gnarly: DurationExtractor, phase 4, 'feedbackButtons3'
         
         sub = [];
         button = [];
@@ -311,11 +451,13 @@ switch measure
            for j = 1:maxTrial
                thisTrial = thisSub(thisSub.TrialID == j, :);
                p4 = thisTrial(thisTrial.TrialPhase == 4, :);
-               p4 = p4.Duration(p4.funcRelevance > 3);    % p4 now contains durations of all p4 fixations to buttons
+               p4 = p4.Duration(p4.funcRelevance > 3);   
+                % p4 now contains durations of all p4 fixations to buttons
                if isempty(p4)
                    timeonfb = NaN;
                else
-                   timeonfb = nansum(p4);   % sum for total time on buttons on this trial
+                    % sum for total time on buttons on this trial
+                   timeonfb = nansum(p4);  
                end
                sub = [sub; current];
                button = [button; timeonfb];
@@ -327,6 +469,8 @@ switch measure
         
     case 'p4feature'        % same as above but features instead of buttons (funcRel = 1, 2, or 3)
         
+         % DurationExtractor, phase 4, 'all3'
+         
         sub = [];
         stim = [];
         
